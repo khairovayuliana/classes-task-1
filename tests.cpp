5b8393e086b381f089ab5de28072bb1297a15096
khairovayuliana <y.khayrova@inbox.ru>
2025-06-02 19:54:18 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,66 +1,84 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "main.cpp"
+#include <sstream>
+#include <iostream>
 
-TEST_CASE("Point: Structure definition and initialization") {
-    Point p;
-    REQUIRE(sizeof(p.x) == sizeof(double));
-    REQUIRE(sizeof(p.y) == sizeof(double));
-}
+class Dragon {
+public:
+    std::string name;
+    int tail_length;
+};
 
-TEST_CASE("Point: Input and output") {
-    SECTION("Simple values") {
-        Point p;
-        std::istringstream input("4.93 5.51");
-        std::ostringstream output;
-        
-        input >> p.x >> p.y;
-        output << p.x << " " << p.y;
-        
-        REQUIRE(output.str() == "4.93 5.51");
+TEST_CASE("Dragon: Class definition and initialization") {
+    SECTION("Type checks") {
+        Dragon d;
+        REQUIRE(std::is_same<decltype(d.name), std::string>::value);
     }
 
-    SECTION("Negative values") {
-        Point p;
-        std::istringstream input("-2.5 3.75");
-        std::ostringstream output;
-        
-        input >> p.x >> p.y;
-        output << p.x << " " << p.y;
-        
-        REQUIRE(output.str() == "-2.5 3.75");
+    SECTION("Direct initialization") {
+        Dragon d{"Lily", 250};
+        REQUIRE(d.name == "Lily");
+        REQUIRE(d.tail_length == 250);
     }
 
-    SECTION("Zero values") {
-        Point p;
-        std::istringstream input("0 0");
-        std::ostringstream output;
-        
-        input >> p.x >> p.y;
-        output << p.x << " " << p.y;
-        
-        REQUIRE(output.str() == "0 0");
-    }
-
-    SECTION("Large values") {
-        Point p;
-        std::istringstream input("123456.789 -987654.321");
-        std::ostringstream output;
-        
-        input >> p.x >> p.y;
-        output << p.x << " " << p.y;
-        
-        REQUIRE(output.str() == "123456.789 -987654.321");
+    SECTION("Field assignment") {
+        Dragon d;
+        d.name = "Toothless";
+        d.tail_length = 17;
+        REQUIRE(d.name == "Toothless");
+        REQUIRE(d.tail_length == 17);
     }
 }
 
-TEST_CASE("Point: Precision check") {
-    Point p;
-    std::istringstream input("3.1415926535 2.7182818284");
+
+TEST_CASE("Basic dragon creation and output") {
+    Dragon d;
+    d.name = "Toothless";
+    d.tail_length = 17;
+    
     std::ostringstream output;
+    output << "Dragon " << d.name << " has a tail of " << d.tail_length << " meter(s)";
     
-    input >> p.x >> p.y;
-    output << std::fixed << std::setprecision(10) << p.x << " " << p.y;
+    REQUIRE(output.str() == "Dragon Toothless has a tail of 17 meter(s)");
+}
+
+TEST_CASE("Input/output simulation") {
+    SECTION("Normal case") {
+        std::istringstream input("Kitty 350");
+        std::ostringstream output;
+        
+        Dragon d;
+        input >> d.name >> d.tail_length;
+        output << "Dragon " << d.name << " has a tail of " << d.tail_length << " meter(s)";
+        
+        REQUIRE(output.str() == "Dragon Kitty has a tail of 350 meter(s)");
+    }
     
-    REQUIRE(output.str() == "3.1415926535 2.7182818284");
+    SECTION("Minimum tail length") {
+        std::istringstream input("MiniDragon 1");
+        Dragon d;
+        input >> d.name >> d.tail_length;
+        output << "Dragon " << d.name << " has a tail of " << d.tail_length << " meter(s)";
+        
+        REQUIRE(output.str() == "Dragon MiniDragon has a tail of 1 meter(s)");
+    }
+    
+    SECTION("Maximum tail length") {
+        std::istringstream input("BigDragon 1000");
+        Dragon d;
+        input >> d.name >> d.tail_length;
+        output << "Dragon " << d.name << " has a tail of " << d.tail_length << " meter(s)";
+        
+        REQUIRE(output.str() == "Dragon BigDragon has a tail of 1000 meter(s)");
+    }
+    
+    SECTION("Name with numbers") {
+        std::istringstream input("Dragon123 500");
+        Dragon d;
+        input >> d.name >> d.tail_length;
+        output << "Dragon " << d.name << " has a tail of " << d.tail_length << " meter(s)";
+        
+        REQUIRE(output.str() == "Dragon Dragon123 has a tail of 500 meter(s)");
+    }
 }
